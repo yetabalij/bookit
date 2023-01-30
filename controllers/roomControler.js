755bb6a11cdff67a5978b364705b91f9a6bcce7c
yetabalij <ermiastsegu@yahoo.com>
2023-01-30 22:22:08 +0300

    create new room is added.

--- controllers/roomControler.js
+++ controllers/roomControler.js
@@ -1,3 +1,5 @@
+import Room from "./../models/rooms";
+
 const allRooms = (req, res) => {
   res.status(200).json({
     success: true,
@@ -5,4 +7,13 @@ const allRooms = (req, res) => {
   });
 };
 
-export { allRooms };
+// create new room => /api/rooms
+const newRoom = async (req, res) => {
+  try {
+    const room = await Room.create(req.body);
+    res.status(200).json({ success: true, room });
+  } catch (error) {}
+  res.status(400).json({ success: false, error: error.message });
+};
+
+export { allRooms, newRoom };
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,11 +1,12 @@
 import nc from "next-connect";
 import dbconnect from "../../../config/dbconnect";
 
-import { allRooms } from "@/controllers/roomControler";
+import { allRooms, newRoom } from "@/controllers/roomControler";
 
 dbconnect();
 
 const handler = nc();
 handler.get(allRooms);
+handler.post(newRoom)
 
 export default handler;
