63a54acc6f2df21f80de26c7c11da1bddcb65833
yetabalij <yetabalij@gmail.com>
2023-04-17 11:43:18 +0300

    db connection added.

--- pages/api/rooms/index.ts
+++ pages/api/rooms/index.ts
@@ -1,8 +1,22 @@
 import nc from "next-connect";
 
 import { allRooms } from "@/pages/controllers/roomController";
+import mongoose from "mongoose";
+
+//db connection
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ermias:ermias@cluster0.t7j5vim.mongodb.net/bookit?retryWrites=true&w=majority"
+    );
+    console.log("connect to db.");
+  } catch (error) {
+    throw error;
+  }
+};
 
 const handler = nc();
+connect();
 
 handler.get(allRooms);
 
