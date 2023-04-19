f1e42e17f724e11973d559195fa0068ba0a957fb
yetabalij <yetabalij@gmail.com>
2023-04-19 10:43:14 +0300

    get all room controller is implemented.

--- controllers/roomController.ts
+++ controllers/roomController.ts
@@ -1,8 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Room from "./../models/rooms";
 
-const allRooms = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ success: true, message: "All Rooms." });
+const allRooms = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json({
+      success: true,
+      rooms,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 //Create new room => /api/rooms
