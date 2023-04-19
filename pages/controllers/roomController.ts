e1434d5fc1008b085d78e01b8b8093769c91f4ee
yetabalij <yetabalij@gmail.com>
2023-04-19 10:37:45 +0300

    create new room controller is created.

--- pages/controllers/roomController.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const allRooms = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ success: true, message: "All Rooms." });
-};
-
-export { allRooms };
