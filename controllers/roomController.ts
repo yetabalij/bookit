f294187769657a968ecc708827c9c7e2f86f6e93
yetabalij <yetabalij@gmail.com>
2023-04-19 12:09:18 +0300

    delete and other functionality implemented.

--- controllers/roomController.ts
+++ controllers/roomController.ts
@@ -55,4 +55,58 @@ const getSingleRoom = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export { allRooms, newRoom, getSingleRoom };
+//update room => /api/rooms:id
+const updateRoom = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    let room = await Room.findById(req.query.id);
+    if (!room) {
+      return res.status(404).json({
+        success: false,
+        error: "room not found.",
+      });
+    }
+
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//delete room => /api/rooms:id
+const deleteRoom = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+      return res.status(404).json({
+        success: false,
+        error: "room not found.",
+      });
+    }
+
+    await Room.findByIdAndDelete(req.query.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Room is deleted.",
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export { allRooms, newRoom, getSingleRoom, updateRoom, deleteRoom };
