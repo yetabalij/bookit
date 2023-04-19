0a6f77979c526fc9a6518899e220aac968b3c4c0
yetabalij <yetabalij@gmail.com>
2023-04-19 12:08:55 +0300

    updated.

--- pages/api/rooms/[id].ts
+++ pages/api/rooms/[id].ts
@@ -1,11 +1,17 @@
 import nc from "next-connect";
 
-import { getSingleRoom } from "@/controllers/roomController";
+import {
+  getSingleRoom,
+  updateRoom,
+  deleteRoom,
+} from "@/controllers/roomController";
 import { connect } from "./../../../utils/dbconnect";
 
 const handler = nc();
 connect();
 
 handler.get(getSingleRoom);
+handler.put(updateRoom);
+handler.delete(deleteRoom);
 
 export default handler;
